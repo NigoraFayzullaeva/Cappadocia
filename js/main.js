6294cb5bac6c4878ccfe7546e7102d7db106879f
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-01-11 15:04:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ $(function(){
     $('.fairy-tail-slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
     });
 });
