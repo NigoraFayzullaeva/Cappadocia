8b89b1828e77f2f9b3029efadff52a6ee0044cbd
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-01-14 13:36:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 601,
+                settings: {
+                    arrows: false
+                }
+            },
+        ]
     });
 });
